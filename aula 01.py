fc05a8e7eaa1e215b94c94554d61bd0cfe5f2296
3morning <madrugadaanderson@gmail.com>
2025-04-06 10:57:09 -0300

    PythonUnasp

---
+++ aula 01.py
@@ -0,0 +1,8 @@
+#print("acordar cedo em dia de domingo! kkkkkkkk")
+#print("seja bem vindo CLT!")
+
+print(type('123'))
+print(type(123))
+print(type([1, 2, 3,]))
+print(type(None))
+print(type(False))
