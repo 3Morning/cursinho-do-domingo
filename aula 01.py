d729cf7ed79b58e649109414b8eb6771a52ef87a
3moning <madrugadaanderson@gmail.com>
2025-04-13 18:34:04 -0300

    aula 2

--- aula 01.py
+++ aula 01.py
@@ -1,6 +1,18 @@
-#print("acordar cedo em dia de domingo! kkkkkkkk")
-#print("seja bem vindo CLT!")
+#obs: na aula 01, aprendemos sobre o comando print() que é mostrar na tela.
+#e os tipos de comandos:
 
+#string("palavra")
+#bool(false or true)
+#list[1, 2, 3, "palavra"]
+#NoneType(none = nulo)
+#int("numero inteiro" ex: 1)
+#float("numero não inteiro" ex: 1.5)
+
+#comando print: 
+print("acordar cedo em dia de domingo! kkkkkkkk")
+print("seja bem vindo CLT!")
+
+#e os tipos de comandos
 print(type('123'))
 print(type(123))
 print(type([1, 2, 3,]))
---
+++ aula 02.py
@@ -0,0 +1,28 @@
+# aula 02
+#Variavel
+#uma variavel é um espaço de armazanamento na memoria de computador que guarda um valor;
+#como numero, texto e outros tipos de dados, e pode ser alterado durantea a execução de um programa;
+
+#input
+#o input é um valor ou palavra que guarda dentro de uma varialvel para que o usuario possa digitar no que desejar
+#e depois exibir na tela com o comando: print("")
+
+#abaixo, apaque as "#"(jogo da velha) para que o codigo funcione
+
+#variavel:
+#palavra = 1
+#print(palavra)
+#palavra = 2
+#print(palavra)
+
+#resultado:
+#1
+#2
+
+#input:
+nome = 'pedrinho'
+ano = 13
+
+print(f"o {nome} tem {ano} de idade")
+msg = f"o {nome} tem {ano} de idade"
+print(msg)
