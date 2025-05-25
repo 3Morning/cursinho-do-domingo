9a66cb531686c27e42bd107b731911c050237376
3morning <madrugadaanderson@gmail.com>
2025-05-25 10:55:38 -0300

    aula 04

---
+++ aula 04.py
@@ -0,0 +1,24 @@
+#tupla
+#tupla = ("a","b","c","d")
+#print(tupla[0])
+#print(tupla[2])
+
+#minha_lista = [10, 20, 30, 'python', 'true']
+#txt = minha_lista[3]
+#print(txt)
+
+#minha_lista[0] = 99
+#print(minha_lista[0])
+#print(minha_lista)
+
+#estrutura de repetições
+contador = 1
+quantidade_ha_repetir = 10
+
+#Enquanto for verdade, ele repetirá
+while contador <= quantidade_ha_repetir:
+    
+    print(f"{contador} - madrugada")
+
+    #variavel de controle
+    contador = contador + 1
